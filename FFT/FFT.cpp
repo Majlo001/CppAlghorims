89450981119ee89c76f9e486c0f3fe1a3a6b28ff
Majlo <szmilosz01@gmail.com>
2023-01-20 10:19:09 +0100

    0.10.1

---
+++ FFT/FFT.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+#include <time.h>
+#include <sstream>
+#include <math.h>
+#include <fstream>
+#include <complex>
+
+#define USE MATH DEFINES
+
+
+
+int main()
+{
+	std::complex<double> z2(2.0, 3.0);
+
+	return 0;
+}
