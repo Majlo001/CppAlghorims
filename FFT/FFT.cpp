9077f3719f811a0db94767f579793cc59171576e
Majlo001 <szmilosz01@gmail.com>
2023-01-24 18:10:05 +0100

    0.10.3

--- FFT/FFT.cpp
+++ FFT/FFT.cpp
@@ -46,22 +46,44 @@ std::complex<double>* fft(double* f, int N) {
 std::complex<double>* dft(double* f, int N) {
 	std::complex<double>* c = new std::complex<double>[N];
 
+	/*for (int i = 0; i < N; i++) {
+		c[i] = 0;
+		for (int k = 0; k < N; k++) {
+			c[i] += f[k] * exp(std::complex<double>(0, -2.0 * i * k * M_PI / N));
+		}
+	}*/
+
 
 	for (int i = 0; i < N; i++) {
 		for (int k = 0; k < N; k++) {
-			//TODO sth here
+			c[i].real(c[i].real() + f[k] * std::cos(2.0 * i * k * M_PI / N));
+			c[i].imag(c[i].imag() - f[k] * std::sin(2.0 * i * k * M_PI / N));
 		}
 	}
 
+	/*std::cout << "DFT:" << std::endl;
+	for (int i = 0; i < N; i++) {
+		std::cout << c[i] << std::endl;
+	}*/
 	return c;
 }
 
 
+double err(std::complex<double>* cDFT, std::complex<double>* cFFT, int N) {
+	double error = 0.0;
+
+	for (int i = 0; i < N; i++) {
+		error += abs(cDFT[i] - cFFT[i]);
+	}
+
+	error = error / N;
+	return error;
+}
+
 
 int main() {
-	//const int MAX_ORDER = 13;
-	const int MAX_ORDER = 4;
-	const bool PRINT_COEFS = true;
+	const int MAX_ORDER = 13;
+	const bool PRINT_COEFS = false;
 	for (int o = 1; o <= MAX_ORDER; o++)
 	{
 		const int N = 1 << o;
@@ -72,26 +94,26 @@ int main() {
 			f[n] = n / (double)N; // probki funkcji liniowej
 
 		clock_t t1 = clock();
-		//std::complex<double>* cDFT = dft(f, N);
+		std::complex<double>* cDFT = dft(f, N);
 		clock_t t2 = clock();
 		double dft_time = (t2 - t1) / (double)CLOCKS_PER_SEC * 1000.0;
 
 
-		//printf("DFT time[ms] : % f \n", dft_time);
+		printf("DFT time[ms] : % f \n", dft_time);
 		t1 = clock();
 		std::complex<double>* cFFT = fft(f, N);
 		t2 = clock();
 		double fft_time = (t2 - t1) / (double)CLOCKS_PER_SEC * 1000.0;
 		printf("FFT time[ms] : % f \n", fft_time);
 
-		//printf("mean error : % f \n", err(cDFT, cFFT, N));
+		printf("Mean error : %.20f \n", err(cDFT, cFFT, N));
 		if (PRINT_COEFS) {
 			for (int k = 0; k < N; k++)
 				std::cout << k << ": " << cFFT[k].real() << " " << cFFT[k].imag() << std::endl;
 			std::cout << "----\n" << std::endl;
 		}
 		delete[] f;
-		//delete[] cDFT;
+		delete[] cDFT;
 		delete[] cFFT;
 	}
 }
